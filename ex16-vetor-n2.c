7e15e4cf853df4ead18e4837d9d153f1a44f1eee
cpmart <cpmart@gmail.com>
2024-04-05 09:10:18 -0300

    vetor ex2 e ex3

---
+++ ex16-vetor-n2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 5
+
+int main(){
+    int vetA[N], vetB[N], vetC[2*N];
+    srand(time(NULL));
+    for(int i=0; i<N; i++){
+        vetA[i]=rand()%51;
+        vetB[i]=rand()%51;
+        printf("%d - %d\n",vetA[i],vetB[i]);
+   }
+   for(int i=0; i<N; i++){
+        vetC[i]=vetA[i];
+        vetC[5+i]=vetB[i];
+   }
+   for(int i=0; i<2*N; i++){
+        printf("%d\n",vetC[i]);
+   }
+}
+
+
+
