936fceafbf363f45c69601c326ec9a56bbcfb36c
cpmart <cpmart@gmail.com>
2024-04-12 09:08:38 -0300

    matriz com arquivo

--- ex20-matriz-03.c
+++ ex20-matriz-03.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-    int matriz[L][C], somaLinha=0, somaColuna=0,maior, totalDiagonalPrincipal=0;
+    int matriz[L][C], somaLinha=0, somaColuna[C]={0},maior, totalDiagonalPrincipal=0;
     srand(time(NULL));
     for (int i = 0; i < L; i++)
     {
@@ -15,23 +15,20 @@ int main()
         {
             matriz[i][j] = rand() % 11;
             somaLinha += matriz[i][j];
+            somaColuna[j] += matriz[i][j];
+            if(i==0 && j==0)
+                maior = matriz[0][0];
+            else
+                maior = matriz[i][j]>maior?matriz[i][j]:maior;
             printf("%3d  ", matriz[i][j]);
         }
         totalDiagonalPrincipal += matriz[i][i];
         printf("| %3d\n",somaLinha);
     }
-    maior = matriz[0][0];
     printf("---------------------\n");
     for (int i = 0; i < C; i++)
-    {   
-        somaColuna=0;
-        for (int j = 0; j < L; j++)
-        {
-            somaColuna += matriz[j][i];
-            maior = matriz[j][i]>maior?matriz[j][i]:maior;
-        }
-        printf("%3d  ",somaColuna);
-    }
+        printf("%3d  ",somaColuna[i]);
+        
     printf("\n\nMaior: %d\n",maior);
     printf("Diagonal principal: %d\n",totalDiagonalPrincipal);
     printf("\n\n");
